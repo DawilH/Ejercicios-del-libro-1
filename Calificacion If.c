0a2f513e40194964b7d5ed5aaa52cd894286f141
DawilH <dawilherrerareyes@gmail.com>
2022-03-03 01:25:49 -0400

    V1

--- Calificacion If.c
+++ Calificacion If.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(void) {
-  float PRO = 0;
-  printf ("Ingrese el promedio del alumno: ");
-  scanf ("%f" , &PRO);
-  if (PRO >= 6)
-printf ("\nAprobado");
+int main(void) {// Inicia el programa
+  float PRO = 0;// Crea una variable y asigna el dato
+  printf ("Ingrese el promedio del alumno: ");// imprime una informacion en la pantalla
+  scanf ("%f" , &PRO);// pide el dato
+  if (PRO >= 6)// condicion con operacion relacional mayor o igual a 6
+printf ("\nAprobado");// imprime el resultado
   return 0;
 }
